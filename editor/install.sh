408b8a285afd203aba205dbe69967adf50126ea4
Kay <uhbonaparte@gmail.com>
2025-04-03 06:58:10 +0000

    Update install.sh

--- editor/install.sh
+++ editor/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create package directory
-mkdir -p /usr/local/share/meth-packages/simpleedit
+mkdir -p /usr/local/share/meth-packages/editor
 
 # Copy Python script to package directory
-cp simpleedit.py /usr/local/share/meth-packages/simpleedit/
+cp simpleedit.py /usr/local/share/meth-packages/editor/
 
 # Make it executable
-chmod +x /usr/local/share/meth-packages/simpleedit/simpleedit.py
+chmod +x /usr/local/share/meth-packages/editor/simpleedit.py
 
 # Create symlink in PATH
-ln -sf /usr/local/share/meth-packages/simpleedit/simpledit.py /usr/local/bin/simpleedit
+ln -sf /usr/local/share/meth-packages/editor/simpledit.py /usr/local/bin/editor
 
 echo "Package Simple Edit installed successfully"
