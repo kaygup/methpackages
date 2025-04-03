0e0accc9237b4360524c206decaffd799583e218
Kay <uhbonaparte@gmail.com>
2025-04-03 07:28:42 +0000

    Create uninstall.sh

---
+++ nox/uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Remove symlink in PATH
+rm -f /usr/local/bin/nox
+# Remove package directory
+rm -rf /usr/local/share/meth-packages/nox
+echo "Package Simple Edit uninstalled successfully"
