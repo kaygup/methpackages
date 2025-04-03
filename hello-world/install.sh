6e2002b5af988d0aed89d41e9b366a8a35d5f602
Kay <uhbonaparte@gmail.com>
2025-04-03 08:40:39 +0000

    Delete hello-world directory

--- hello-world/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create symlink to make hello-world executable from command line
-ln -sf /usr/local/meth/packages/hello-world/hello-world.py /usr/local/bin/hello-world
-chmod +x /usr/local/meth/packages/hello-world/hello-world.py
-chmod +x /usr/local/bin/hello-world
-
-echo "hello-world installed successfully!"
-echo "Run 'hello-world' to test it"
