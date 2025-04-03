0fea893e892c7a61bb217d5f4d45d8a8b609fa03
Kay <uhbonaparte@gmail.com>
2025-04-03 17:56:52 +0000

    Update install.sh

--- vesktop/install.sh
+++ vesktop/install.sh
@@ -1,6 +1,2 @@
-git clone https://github.com/Vencord/Vesktop
-cd Vesktop
-sudo pacman -S npm
-npm install --global pnpm
-pnpm i
-pnpm package
+sudo pacman -S flatpak
+flatpak install flathub dev.vencord.Vesktop
