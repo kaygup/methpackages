39bb491a04276f2de96c115a555cd0c685107993
Kay <uhbonaparte@gmail.com>
2025-04-03 13:55:45 +0000

    Create uninstall.sh

---
+++ waterfall/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove symlink
+rm -f /usr/local/bin/waterfall
+
+# Remove package files
+rm -rf /usr/local/share/meth-packages/waterfall
+
+echo "Terminal Waterfall animation uninstalled successfully"
