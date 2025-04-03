e45ecd2f1900de7b66de67aa19b6f024ef32f390
Kay <uhbonaparte@gmail.com>
2025-04-03 09:02:15 +0000

    Delete tux directory

--- tux/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remove symlink from PATH
-rm -f /usr/local/bin/tux
-
-# Remove package directory and its contents
-rm -rf /usr/local/share/meth-packages/tux
-
-echo "Package ASCII Penguin uninstalled successfully"
